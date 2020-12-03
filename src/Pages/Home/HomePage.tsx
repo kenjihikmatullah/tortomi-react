517a9b8262f36fb2546f9b0533181ce8907b20f7
Kenji Hikmatullah <iniemailkenji@gmail.com>
2020-12-03 14:05:08 +0700

    improve homepage

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,17 +1,38 @@
+import { List } from "@fluentui/react";
 import React from "react";
+import { connect } from "react-redux";
+import { GlobalProps, mapDispatchToProps, mapStateToProps } from "Redux/Map";
 import { WithNavbar } from '../../Components';
-import { Text } from 'office-ui-fabric-react/lib/Text';
+import ArticleItem from "../Article/ArticleList/ArticleItem/ArticleItem";
 
-class HomePage extends React.Component {
+class HomePage extends React.Component<GlobalProps> {
   static route = '/';
 
   render() {
+    const { articles } = this.props;
+
     return (
       <WithNavbar>
-        <Text>Halo</Text>
+
+        {/* TODO: headline */}
+
+        <List
+          items={articles}
+          onRenderCell={(item, index) => {
+            return (
+              <div style={{ marginTop: index != 0 ? 20 : 0 }}>
+                <ArticleItem
+                  article={item}
+                />
+              </div>
+            );
+          }}
+        />
       </WithNavbar>
     )
   }
 }
 
-export default HomePage;
+export default connect(mapStateToProps, mapDispatchToProps)(
+  HomePage
+);
