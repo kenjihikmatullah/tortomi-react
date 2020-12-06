3f26d15a7207bcf174bcb651c0674cbfad6be984
Kenji Hikmatullah <iniemailkenji@gmail.com>
2020-12-06 21:18:27 +0700

    about us

--- src/Pages/About/AboutUsPage.tsx
+++ src/Pages/About/AboutUsPage.tsx
@@ -3,6 +3,7 @@ import { WithNavbar } from 'Components';
 import React, { FunctionComponent as Component } from "react";
 import { connect } from "react-redux";
 import { GlobalProps, mapDispatchToProps, mapStateToProps } from "Redux/Map";
+import { DEFAULT_ARTICLE_IMAGE } from "Utils/constants";
 import './AboutUsPage.scss';
 
 const AboutUsPage: Component<GlobalProps> = (props: GlobalProps) => {
@@ -14,7 +15,14 @@ const AboutUsPage: Component<GlobalProps> = (props: GlobalProps) => {
           Tentang Kami
         </Text>
 
-        <Text className='value'>
+        {/* Image */}
+        <img
+          src={DEFAULT_ARTICLE_IMAGE}
+          alt={`Image of Tortomi`}
+          className='image'
+        />
+
+        <Text className='value' block>
           Tortomi adalah pusat informasi tentang kura-kura dan salah satu yang terbaik di Indonesia. Tortomi berdiri pada tahun 2020 di Surabaya dengan tujuan awal ingin menyediakan informasi dengan mudah tentang cara mengobati berbagai penyakit pada kura-kura.
         </Text>
 
